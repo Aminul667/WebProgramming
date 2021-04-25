37f20e1f7436fd865da5d224c40331811e2d7f2b
Aminul667 <rahathssn@gmail.com>
2021-04-25 20:47:22 +0600

    dictionary

---
+++ Python/dictionaries.py
@@ -0,0 +1,11 @@
+houses = {'Harry': 'Gryffindor', 'Draco':'Slytherin'}
+
+houses['Hermione'] = 'Gryffindor'
+
+print(houses)
+
+ages = {"Alice": 22, "Bob": 27}
+ages["Charlie"] = 30
+ages["Alice"] += 1
+
+print(ages)
